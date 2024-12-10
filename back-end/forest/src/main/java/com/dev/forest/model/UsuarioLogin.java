33915752030128b8b81e4268e73e951a991950f3
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-12-09 21:04:35 -0300

    Criando a lógica de autenticação na camada de service

---
+++ back-end/forest/src/main/java/com/dev/forest/model/UsuarioLogin.java
@@ -0,0 +1,80 @@
+package com.dev.forest.model;
+
+public class UsuarioLogin {
+
+	private Long id;
+
+	private String nome;
+
+	private String email;
+
+	private String senha;
+
+	private String foto;
+
+	private String token;
+	
+	private String tipo;
+	
+	
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}	
+	
+}
