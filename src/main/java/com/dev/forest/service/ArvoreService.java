81622ee3fc6e3c90545d8c19bd4721b6fea4e88b
Marcos Vinicius <contato.marcoscandido@gmail.com>
2025-04-24 15:05:05 -0300

    Backend continuação

---
+++ src/main/java/com/dev/forest/service/ArvoreService.java
@@ -0,0 +1,16 @@
+package com.dev.forest.service;
+
+import com.dev.forest.model.Arvore;
+
+public interface ArvoreService  {
+	
+	
+	    void calcularHoraTermino(Arvore arvore);
+
+	    
+	    
+    
+	    
+	
+
+}
